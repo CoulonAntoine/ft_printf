b9d681888fdf10d708ea7505d4c11338ab44cec1
Antoine Coulon <ancoulon@student.s19.be>
2020-02-27 14:31:29 +0100

    finished X (uppercase hexadecimal)

--- sources/ft_print_ux.c
+++ sources/ft_print_ux.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_print_ux.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ancoulon <ancoulon@student.s19.be>         +#+  +:+       +#+        */
+/*   By: ancoulon <ancoulon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/13 12:18:43 by ancoulon          #+#    #+#             */
-/*   Updated: 2020/02/13 15:40:16 by ancoulon         ###   ########.fr       */
+/*   Updated: 2020/02/27 14:30:49 by ancoulon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,18 @@
 
 void		ft_print_ux(t_format *fmt, va_list *va, t_int32 *ret)
 {
-	(void)fmt;
-	(void)va;
-	(void)ret;
-	ft_putchar_fd('X', 1);
+	t_uint64	arg;
+	t_int32		pad;
+
+	arg = (t_uint64)va_arg(*va, unsigned int);
+	*ret += ft_hexsize(fmt, arg);
+	if (!(fmt->flag & FLAG_WIDTH))
+		return (ft_printhexnbr(fmt, arg, UHEX_BASE));
+	pad = (t_int32)fmt->width - (t_int32)ft_hexsize(fmt, arg);
+	pad = (pad >= 0) ? pad : 0;
+	if (fmt->flag & FLAG_LEFT)
+		ft_printhexnbr(fmt, (t_int64)arg, UHEX_BASE);
+	ft_printpad(fmt, pad, ret);
+	if (!(fmt->flag & FLAG_LEFT))
+		ft_printhexnbr(fmt, (t_int64)arg, UHEX_BASE);
 }
